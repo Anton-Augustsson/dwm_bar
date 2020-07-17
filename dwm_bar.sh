1558e35a71565ae0b757e50a228581cc8df10136
anton <anton.augustsson99@gmail.com>
2020-07-17 08:52:24 +0000

    disk

---
+++ dwm_bar.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Store the directory the script is running from
+LOC=$(readlink -f "$0")
+DIR=$(dirname "$LOC")
+
+# Requires a font with adequate unicode character support
+export IDENTIFIER="unicode"
+
+# Change the charachter(s) used to seperate modules. If two are used, they will be placed at the start and end.
+export SEP1="["
+export SEP2="]"
+
+# Import the modules
+. "$DIR/bar-functions/dwm_battery.sh"
+. "$DIR/bar-functions/dwm_alsa.sh"
+. "$DIR/bar-functions/dwm_weather.sh"
+. "$DIR/bar-functions/dwm_networkmanager.sh"
+. "$DIR/bar-functions/dwm_date.sh"
+
+# Update dwm status bar every second
+while true
+do
+    xsetroot -name "$(dwm_battery)$(dwm_alsa)$(dwm_networkmanager)$(dwm_date)"
+    sleep 1
+done
